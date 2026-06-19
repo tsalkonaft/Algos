cdcf938021a13157f0aafcbac330250fac11deae
tsalkonaft <tsalkonaft@gmail.com>
2026-06-19 11:03:06 +0200

    Added to Git

---
+++ src/test/java/org/tsalko/leetcode/LeetCOde013_Test.java
@@ -0,0 +1,26 @@
+package org.tsalko.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class LeetCOde013_Test {
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                Arguments.of("III", 3),
+                Arguments.of("LVIII", 58),
+                Arguments.of("MCMXCIV", 1994)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    public void test(String roman, int expected) {
+        Assertions.assertEquals(expected, new LeetCode013_RomanToInteger().romanToInt(roman));
+    }
+
+
+}
